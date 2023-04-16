11cb9739629428f964dc8ef4d855cc95d4f418de
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2023-04-16 20:14:28 +0200

    Add output file, its shared memory and semaphores

--- proj2.c
+++ proj2.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>   // for random
-#include <unistd.h> // for POSIX types
+#include <time.h>     // for get_random_from_range()
+#include <unistd.h>   // for POSIX types
+#include <stdarg.h>   // for my_print()
+#include <sys/mman.h> // for shared memory
+#include <fcntl.h>    /* For O_* constants */
+#include <sys/stat.h> /* For mode constants */
+#include <semaphore.h>
+
+// wtf idk inspirace z interfernetu
+#define MMAP(ptr)                                                                                  \
+  {                                                                                                \
+    (ptr) = mmap(NULL, sizeof(*(ptr)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); \
+  }
+#define UNMAP(ptr)                 \
+  {                                \
+    munmap((ptr), sizeof(*(ptr))); \
+  }
+// end of wtf idk inspirace z interfernetu
 
 // Define (pointer) global variable for output file
 FILE *output_file;
+int *row_number;
+sem_t *output;
 
 // Intialize global parameters variables
 int NZ = -1;
@@ -46,9 +64,17 @@ useconds_t get_random_from_range(int min, int max)
   return min + (rand() % (max - min + 1));
 }
 
-int main(int argc, char *argv[])
+void prepare_output()
 {
-  parse_params(argc, argv);
+  // Create shared memory
+  MMAP(output);
+
+  // Init semaphore
+  sem_init(output, 1, 1);
+
+  // Create shared memories
+  MMAP(row_number);
+  MMAP(output_file);
 
   // Open output file
   output_file = fopen("proj2.out", "w");
@@ -57,11 +83,65 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Cannot open file.");
     exit(1);
   }
+}
 
-  usleep(get_random_from_range(F / 2, F));
-
+void cleanup_output()
+{
   // Close output file
   fclose(output_file);
 
+  // Destroy shared memories
+  UNMAP(output_file);
+  UNMAP(row_number);
+
+  // Destroy semaphore and their shared memory
+  sem_destroy(output);
+  UNMAP(output);
+}
+
+void my_print(const char *format, ...)
+{
+  sem_wait(output);
+
+  // Get function params
+  va_list args;
+  va_start(args, format);
+
+  // Print number of row
+  fprintf(output_file, "%d: ", ++(*row_number));
+
+  // Print the string from params
+  vfprintf(output_file, format, args);
+
+  // Flush buffer
+  fflush(output_file);
+
+  // Cleanup function params
+  va_end(args);
+
+  sem_post(output);
+}
+
+int main(int argc, char *argv[])
+{
+  parse_params(argc, argv);
+  prepare_output();
+
+  // Hlavní proces vytváří ihned po spuštění NZ procesů zákazníků a NU procesů úředníků
+
+  // Čeká pomocí volání usleep náhodný čas v intervalu <F/2,F>
+  usleep(get_random_from_range(F / 2, F));
+
+  // Vypíše: A: closing
+  my_print("closing\n");
+
+  // Poté čeká na ukončení všech procesů, které aplikace vytváří.
+  // Wait for children suicide
+  // while (wait(NULL) > 0)
+  //   ;
+
+  cleanup_output();
+
+  // Jakmile jsou tyto procesy ukončeny, ukončí se i hlavní proces s kódem (exit code) 0.
   return 0;
 }
